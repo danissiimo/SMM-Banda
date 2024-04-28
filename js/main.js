236317f31d0340c53757c0b3ca8fc3394fe39439
danisimo <danco@inbox.ru>
2024-04-28 23:34:10 +0300

    h2>=2

--- js/main.js
+++ js/main.js
@@ -220,7 +220,7 @@ $(document).ready(function(){
     
     //// ToC
     
-    if($(".article-content").length){
+    if($(".article-content").length && $(".article-content h2").length>=2){
         $(".article-content h2").first().after('<p>Контент:</p> <ul class="toc" data-toc="div.article-content" data-toc-headings="h2,h3,h4"></ul>');
     }
 });
